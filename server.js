039fae3b958b0b18e5951f4af5abc19aee68c143
Samantha De Waal Malefyt <samanthadwm@gmail.com>
2017-07-01 18:59:16 -0500

    updated server.js

--- server.js
+++ server.js
@@ -30,12 +30,51 @@ db.once("open", function() {
   console.log("Mongoose connection successful.");
 });
 
-
 //ROUTES
+app.get('/', function(req, res){
+	res.sendFile(__dirname + './public/index.html');
+});
 
+app.post('/api/saved', function(req, res){
+	var newArticle = new Articles(req.body);
 
+	var title = req.body.title;
+	var date = req.body.date;
+	var url = req.body.url;
 
+	newArticle.save(function(err, doc){
+		if(err){
+			console.log("post error");
+		}
+		else{
+			res.send(doc._id);
+		}
+	});
+});
 
+app.get('api/saved', function(req, res){
+	Articles.find({}).exec(function(err, doc){
+		if(err){
+			console.log("get error");
+		}
+		else {
+			res.json(doc);
+		}
+	});
+});
+
+app.delete('api/saved/', function(req, res){
+	var url = req.param('url');
+
+	Articles.find({"url": url}).remove().exec(function(err, data){
+		if(err){
+			console.log("delete error");
+		}
+		else{
+			res.send("deleted");
+		}
+	});
+});
 
 // Listener
 app.listen(PORT, function() {
