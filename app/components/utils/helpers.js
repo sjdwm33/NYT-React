d6a188d61cc51b81f2b6461bdc9e288ee3011c49
Samantha De Waal Malefyt <samanthadwm@gmail.com>
2017-07-01 20:45:07 -0500

    updated helpers.js

--- app/components/utils/helpers.js
+++ app/components/utils/helpers.js
@@ -1,25 +1,33 @@
 var axios = require("axios");
+var nytimesAPI = "4f14f6be03334883a5c41c827be80938";
 
 var helper = {
 
+  //This function runs the query to NYT
+  runQuery: function(topic, startYear, endYear){
+  	var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + nytimesAPI + "&q=" + topic + "&begin_date=" + startYear + "0101&end_date=" + endYear + "0101";
+  	return axios.get(queryURL).then(function(res){
+  		var newResults = [];
+  		var allResults = res.data.res.docs;
+  		var counter = 0;
 
+  		//return the first 5 articles that meet the criteria
+  		for (var i = 0; i < fullReults.length; i++) {
+  			if (counter > 4) {
+  				return newResults;
+  			}
 
-
-
-
-
-
-
-
-
-  // This function hits our own server to retrieve the record of query results
-  getSaved: function() {
-    return axios.get("/api");
+  			if(fullReults[counter].headline.main && fullReults[counter].pub_date && fullReults[counter].web_url) {
+  				counter++;
+  			}
+  		}
+  		return newResults;
+  	})
   },
 
   // This function posts saved articles to our database.
-  postSaved: function(location) {
-    return axios.post("/api", { title: title, url: url });
+  postArticle: function(title, date, url) {
+    return axios.post("/api", { title: title, date: date, url: url});
   }
 
 };
